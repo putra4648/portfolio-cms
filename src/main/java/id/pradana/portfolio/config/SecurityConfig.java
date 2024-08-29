02a98f982e2c04e8e3beb43826a5f8dce225987a
Walkers <wakwaujaim@gmail.com>
2024-08-29 06:19:04 +0000

    test: use dummy login

--- src/main/java/id/pradana/portfolio/config/SecurityConfig.java
+++ src/main/java/id/pradana/portfolio/config/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig {
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(Customizer.withDefaults());
         return http.build();
--- src/main/java/id/pradana/portfolio/controller/HelloController.java
+++ src/main/java/id/pradana/portfolio/controller/HelloController.java
@@ -1,6 +1,9 @@
 package id.pradana.portfolio.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
 
     @GetMapping("/")
-    public ModelAndView indexPage() {
+    public ModelAndView indexPage(Model model, @AuthenticationPrincipal OidcUser principal) {
         return new ModelAndView("index.html");
     }
 
