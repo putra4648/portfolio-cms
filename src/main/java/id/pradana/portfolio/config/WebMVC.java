15d965bdb891189d368b0ce2bf82adadd96488d6
Walkers <wakwaujaim@gmail.com>
2024-08-29 06:08:40 +0000

    feat: added auth0 jwt

--- src/main/java/id/pradana/portfolio/config/WebMVC.java
+++ src/main/java/id/pradana/portfolio/config/WebMVC.java
@@ -1,13 +1,14 @@
 package id.pradana.portfolio.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMVC implements WebMvcConfigurer {
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/webjars/**")
                 .addResourceLocations("/webjars/")
